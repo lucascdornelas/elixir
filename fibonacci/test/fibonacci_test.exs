7194f17582272f0d3ca7bfec1884e3efc6e6c88f
Lucas Cota <lucascdornelas@gmail.com>
2021-08-16 16:18:36 -0300

    test: add unit test for fib function

--- fibonacci/test/fibonacci_test.exs
+++ fibonacci/test/fibonacci_test.exs
@@ -2,7 +2,35 @@ defmodule FibonacciTest do
   use ExUnit.Case
   doctest Fibonacci
 
-  test "greets the world" do
-    assert Fibonacci.hello() == :world
+  test "return fib element 0 correctly" do
+    assert Fibonacci.fib(0) == 0
+  end
+
+  test "return fib element 1 correctly" do
+    assert Fibonacci.fib(1) == 1
+  end
+
+  test "return fib element 2 correctly" do
+    assert Fibonacci.fib(2) == 1
+  end
+
+  test "return fib element 3 correctly" do
+    assert Fibonacci.fib(3) == 2
+  end
+
+  test "return fib element 4 correctly" do
+    assert Fibonacci.fib(4) == 3
+  end
+
+  test "return fib element 5 correctly" do
+    assert Fibonacci.fib(5) == 5
+  end
+
+  test "return fib element 6 correctly" do
+    assert Fibonacci.fib(6) == 8
+  end
+
+  test "return fib element 7 correctly" do
+    assert Fibonacci.fib(7) == 13
   end
 end
